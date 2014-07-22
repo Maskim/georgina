37bb7f5d06a7aaaa15f7f07010ac22b9b9585501
Maxime Hersand <mhersand@clever-age.com>
2014-07-22 22:38:09 +0200

    create left menu

---
+++ index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<!--[if lte IE 7]> <html class="no-js ie67 ie678" lang="fr"> <![endif]-->
+<!--[if IE 8]> <html class="no-js ie8 ie678" lang="fr"> <![endif]-->
+<!--[if IE 9]> <html class="no-js ie9" lang="fr"> <![endif]-->
+<!--[if gt IE 9]> <!--><html class="no-js" lang="fr"> <!--<![endif]-->
+<head>
+    <meta charset="UTF-8">
+    <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
+    <title>Georgina Collier</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="keyword" content="Georgina, Collier, graphiste, videaste, graphic, designer, video" />
+
+    <!--[if lt IE 9]>
+    <![endif]-->
+    <link rel="stylesheet" href="core/css/styles.css" media="all">
+    <link rel="stylesheet" href="core/css/font-awesome.css" media="all">
+</head>
+<body>
+
+<nav>
+    <h1><a href="/" title="Georgina Collier" data-title="Georgina Collier">GC</a></h1>
+
+    <ul>
+        <li>
+            <a href="#" data-title="Home">
+                <i class="fa fa-home"></i>
+                <span>Home</span>
+
+                <div class="link-alt">Home</div>
+            </a>
+        </li>
+        <li>
+            <a href="#" data-title="Films">
+                <i class="fa fa-film"></i>
+                <span>Movies</span>
+            </a>
+        </li>
+        <li>
+            <a href="#" data-title="Photos">
+                <i class="fa fa-photo"></i>
+                <span>Photos</span>
+            </a>
+        </li>
+        <li>
+            <a href="#" data-title="Contact">
+                <i class="fa fa-phone"></i>
+                <span>Contact</span>
+            </a>
+        </li>
+    </ul>
+</nav>
+
+</body>
+</html>
