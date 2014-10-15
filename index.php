59b4af393455535971e4aee4385e562f5730f312
Maxime Hersand <mhersand@clever-age.com>
2014-10-15 09:24:18 +0200

    mise en place des sections

--- index.php
+++ index.php
@@ -17,60 +17,99 @@
     <link rel="stylesheet" href="core/css/font-awesome.css" media="all">
 </head>
 <body>
-
-<!--<nav>
-    <h1>
-        <a href="/" title="Georgina Collier" data-title="Georgina Collier">GC</a>
-
-        <div class="link-alt">Georgina Collier</div>
-    </h1>
-
-    <ul>
-        <li>
-            <a href="#" data-title="Home">
-                <i class="fa fa-home"></i>
-                <span>Home</span>
-
-            </a>
-            <div class="link-alt">Home</div>
-        </li>
-        <li>
-            <a href="#" data-title="Films">
-                <i class="fa fa-film"></i>
-                <span>Movies</span>
-
-            </a>
-            <div class="link-alt">My movies</div>
-        </li>
-        <li>
-            <a href="#" data-title="Photos">
-                <i class="fa fa-photo"></i>
-                <span>Photos</span>
-            </a>
-
-            <div class="link-alt">My prints projects</div>
-        </li>
-        <li>
-            <a href="#" data-title="Contact">
-
-                <i class="fa fa-phone"></i>
-                <span>Contact</span>
-            </a>
-            <div class="link-alt">How to contact me ?</div>
-        </li>
-    </ul>
-</nav>
--->
-
 <div class="home">
     <h1 class="me">
         <span class="left">Georgina</span>
         <span class="right">Collier</span>
     </h1>
 
-    <div class="designer">Designer</div>
-    <div class="videaste">Videaste</div>
+    <div class="designer">
+        <span>Designer</span>
+    </div>
+    <div class="videaste">
+        <span>Vidéaste</span>
+    </div>
+
+    <nav>
+        <!--<h1>
+            <a href="/" title="Georgina Collier" data-title="Georgina Collier">GC</a>
+
+            <div class="link-alt">Georgina Collier</div>
+        </h1>-->
+
+        <ul>
+            <li>
+                <a href="#home" data-title="Home">
+                    <i class="fa fa-home"></i>
+                    <span>Home</span>
+
+                </a>
+                <!--<div class="link-alt">Home</div>-->
+            </li>
+            <li>
+                <a href="#designer" data-title="Photos">
+                    <i class="fa fa-photo"></i>
+                    <span>Designer</span>
+                </a>
+
+                <!--<div class="link-alt">My prints projects</div>-->
+            </li>
+            <li>
+                <a href="#videaste" data-title="Films">
+                    <i class="fa fa-film"></i>
+                    <span>Vidéaste</span>
+
+                </a>
+                <!--<div class="link-alt">My movies</div>-->
+            </li>
+            <li>
+                <a href="#contact" data-title="Contact">
+                    <i class="fa fa-phone"></i>
+                    <span>Contact</span>
+                </a>
+                <!--<div class="link-alt">How to contact me ?</div>-->
+            </li>
+        </ul>
+    </nav>
 </div>
+
+<section id="me">
+    <h2><span>What about me ?</span></h2>
+</section>
+
+<section id="designer">
+    <h2><span>Designer</span></h2>
+</section>
+
+<section id="videaste">
+    <h2><span>Vidéaste</span></h2>
+</section>
+
+<section id="contact">
+    <h2><span>How to contact me ?</span></h2>
+    <div class="content">
+        <form action="#" method="POST">
+            <div class="form-group">
+                <label for="subject">Subject</label>
+                <input type="text" id="subject" name="subject" />
+            </div>
+
+            <div class="form-group">
+                <label for="subject">Your email</label>
+                <input type="email" id="email" name="email" />
+            </div>
+
+            <div class="form-group">
+                <label for="content">Your message</label>
+                <textarea id="content"></textarea>
+            </div>
+
+            <div>
+                <input type="submit" value="Envoyer" />
+            </div>
+        </form>
+    </div>
+</section>
 
 <script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
 
