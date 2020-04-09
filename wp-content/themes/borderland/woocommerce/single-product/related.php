5d4218c2e53dca1b642fd379b354f9f8c56a80f5
Maxime Hersand <maxime.hersand@gmail.com>
2020-04-09 10:57:48 +0200

    update theme

--- wp-content/themes/borderland/woocommerce/single-product/related.php
+++ wp-content/themes/borderland/woocommerce/single-product/related.php
@@ -10,53 +10,33 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see 	    https://docs.woothemes.com/document/template-structure/
- * @author 		WooThemes
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
  * @package 	WooCommerce/Templates
- * @version     1.6.4
+ * @version     3.0.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 }
 
-global $product, $woocommerce_loop;
-
-if ( empty( $product ) || ! $product->exists() ) {
-    return;
-}
-
-if ( ! $related = $product->get_related( $posts_per_page ) ) {
-    return;
-}
-
-$args = apply_filters( 'woocommerce_related_products_args', array(
-	'post_type'				=> 'product',
-	'ignore_sticky_posts'	=> 1,
-	'no_found_rows' 		=> 1,
-	'posts_per_page' 		=> $posts_per_page,
-	'orderby' 				=> $orderby,
-	'post__in' 				=> $related,
-	'post__not_in'			=> array( $product->id )
-) );
-
-$products                    = new WP_Query( $args );
-$woocommerce_loop['name']    = 'related';
-$woocommerce_loop['columns'] = apply_filters( 'woocommerce_related_products_columns', $columns );
-
-if ( $products->have_posts() ) : ?>
+if ( $related_products ) : ?>
 
 	<div class="related products">
 
-		<h4 class="related-products-title"><?php _e( 'Related Products', 'woocommerce' ); ?></h4>
+		<h4 class="related-products-title"><?php esc_html_e( 'Related Products', 'borderland' ); ?></h4>
 
 		<?php woocommerce_product_loop_start(); ?>
-
-			<?php while ( $products->have_posts() ) : $products->the_post(); ?>
-
-				<?php wc_get_template_part( 'content', 'product' ); ?>
-
-			<?php endwhile; // end of the loop. ?>
+		
+		<?php foreach ( $related_products as $related_product ) : ?>
+			
+			<?php
+			$post_object = get_post( $related_product->get_id() );
+			
+			setup_postdata( $GLOBALS['post'] =& $post_object );
+			
+			wc_get_template_part( 'content', 'product' ); ?>
+		
+		<?php endforeach; ?>
 
 		<?php woocommerce_product_loop_end(); ?>
 
--- wp-content/themes/borderland/woocommerce/single-product/tabs/additional-information.php
+++ wp-content/themes/borderland/woocommerce/single-product/tabs/additional-information.php
@@ -10,10 +10,10 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see 	    https://docs.woothemes.com/document/template-structure/
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
  * @author        WooThemes
  * @package       WooCommerce/Templates
- * @version       2.0.0
+ * @version       3.0.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -22,8 +22,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $product;
 
-$heading = apply_filters( 'woocommerce_product_additional_information_heading', __( 'Additional Information', 'woocommerce' ) );
-
-?>
-
-<?php $product->list_attributes(); ?>
+do_action( 'woocommerce_product_additional_information', $product );
